f5d1f66f296873d2152971ea004849f662bb2091
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-06 05:25:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr.Chocolate', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( nil, 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr.Chocolate', 9, 'male', 'brown', 'cautious', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 9, 'male', 'brown', 'cautious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 9, 'female', 'brown', 'cautious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'male', 'brown', 'cautious', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 9, 'female', 'brown', 'cautious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'male', 'brown', 'cautious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'female', 'brown', 'cautious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( nil, 9, 'male', 'brown', 'cautious', 0);
