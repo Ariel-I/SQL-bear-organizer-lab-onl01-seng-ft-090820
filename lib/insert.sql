e0a05a950788eed8355126f8003b48a41cd485a1
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-06 05:02:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky'
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( 'nil', 9, 'male', 'brown', 'cautious', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( nil, 9, 'male', 'brown', 'cautious', true)
