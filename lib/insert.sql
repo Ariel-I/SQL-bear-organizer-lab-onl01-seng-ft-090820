d0ea265e2d8919b5b479c418e32263efe30af242
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-06 05:22:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr.Chocolate', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'male', 'brown', 'cautious', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( nil, 9, 'male', 'brown', 'cautious', true);
