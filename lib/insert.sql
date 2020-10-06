eaed609cd6d8736d6a572dea22b1bb5548f8d979
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-06 05:30:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,4 @@
-#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Mr.Chocolate', 9, 'M', 'brown', 'cautious', 0);
-#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Rowdy', 9, 'M', 'brown', 'cautious', 1);
-#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Tabitha', 9, 'F', 'brown', 'cautious', 1);
-#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M', 'brown', 'cautious', 0);
-#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Melissa', 9, 'F', 'brown', 'cautious', 1);
-#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'brown', 'cautious', 1);
-#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'F', 'brown', 'cautious', 1);
-#INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( nil, 9, 'M', 'brown', 'cautious', 0);
+
 
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
