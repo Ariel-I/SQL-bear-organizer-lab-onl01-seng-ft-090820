31fb2e148bb9fda830e2ab4a6ea13132b38f9a17
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-06 04:54:32 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bears (id, name, age, gender, color, temperament, alive);
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN);
