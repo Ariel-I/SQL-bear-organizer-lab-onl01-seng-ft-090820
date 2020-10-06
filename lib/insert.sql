de3f0e5f5d89245c6e3b5a2a4abc608b4d1728c7
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-06 04:59:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
