11252a716e9a6e9c5fc7ff506133991f07cce0d5
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-06 05:06:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( nil, 9, 'male', 'brown', 'cautious', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smoky', 9, 'male', 'brown', 'cautious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( nil, 9, 'male', 'brown', 'cautious', true);
